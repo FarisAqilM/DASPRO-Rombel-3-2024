26f9b2b3eef96f54749efb652883adca39571d38
FarisAqilM <farisaqilmakruf01@students.unnes.ac.id>
2024-10-08 15:40:14 +0700

    Update 2-H.cpp

---
+++ Tugas TLX/2405100084_FARIS AQIL MA'RUF/2-H.cpp
@@ -0,0 +1,15 @@
+//Apakah keluaran dari program berikut?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = x;
+    y = 5;
+    cout << x << endl;
+}
+
+//Jawabannya adalah 10 
+//karna nilai dari Y = X 
+//Yang dimana nilai X adalah 10
