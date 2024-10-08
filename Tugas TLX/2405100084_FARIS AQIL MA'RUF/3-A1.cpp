044fcd17bafd9e90d2c4ba04aa443aee7fa89ed3
FarisAqilM <farisaqilmakruf01@students.unnes.ac.id>
2024-10-08 15:59:12 +0700

    Update 3-A1.cpp

---
+++ Tugas TLX/2405100084_FARIS AQIL MA'RUF/3-A1.cpp
@@ -0,0 +1,16 @@
+// Apakah keluaran dari program berikut?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 3, y = 4;
+
+    x -= 1;
+    y += x * 5;
+
+    cout << y << endl;
+}
+
+// Hasil keluaran dari Y adalah 14
+// Karna 4 + 2 x 5
